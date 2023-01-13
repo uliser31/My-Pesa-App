59aebae98e1c6cc81d743b04913041ce762a9d12
Uliser Barrero Rodriguez <uliser.barrero@desoft.cu>
2023-01-13 13:03:16 -0500

    Archivos segunda configuracion

---
+++ src/app/primercomponente/primer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'hello-word',
+    template: '<h1>Mi 2 hola mundo</h1>',
+    styles: ['h1 {background:#001; color: #fff}']
+})
+export class HolaMundo{};
