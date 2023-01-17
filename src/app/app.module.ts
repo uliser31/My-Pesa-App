68b5de0174437715bbc773be3d64a79ef96c9d5a
Uliser Barrero Rodriguez <uliser.barrero@desoft.cu>
2023-01-17 16:11:43 -0500

    Avanzando en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HolaMundo} from './primercomponente/primer.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaMundo
+    HolaMundo,
+    UserComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,11 @@
+import { Text } from '@angular/compiler';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  @Input() nameUser='';
+}
